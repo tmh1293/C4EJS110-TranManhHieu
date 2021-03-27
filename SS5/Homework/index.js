3db2adc78bf058b4944a4ea9ac444287e47254d1
tmh1293 <tmh.101293@gmail.com>
2021-03-28 00:56:32 +0700

    update ss5

---
+++ SS5/Homework/index.js
@@ -0,0 +1,11 @@
+function loadskill()
+{
+    for(i=0;i<=100;i++)
+    {
+        document.getElementById("loadskill").value = i;
+        setTimeout(() => {
+            
+        }, 100);
+    }
+}
+loadskill()
